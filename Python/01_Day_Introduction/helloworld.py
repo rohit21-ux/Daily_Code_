a239c8cd7032625d2b488cb32952aff7c98ef97f
Rohit Jagave <jagaverohit2@gmail.com>
2025-12-16 12:10:45 +0530

    Add Python Day 01 helloworld program

---
+++ Python/01_Day_Introduction/helloworld.py
@@ -0,0 +1,16 @@
+#introduction
+
+print("hello world!")
+
+#30 days of python programming
+
+# Operators in python 
+
+print( 5 + 6)   # addition (+)
+print(5 - 6)    # subtraction (-)
+print(5 * 6)    # multiplication (*)
+print(5 / 6)    # division (/)
+print(5 **6 )   # exponential (**)
+print(5 % 6)    # modulus (%)
+print (5 // 6)  # Floor division operator (//)
+
