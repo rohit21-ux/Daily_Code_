10861764146a37f7792ffe78f6704f259c677e4e
Rohit Jagave <jagaverohit2@gmail.com>
2026-01-22 22:28:26 +0530

    Daily Code

---
+++ Python/12-Day-Class&Object/class.py
@@ -0,0 +1,42 @@
+'''Class is like a blueprint for creating a object.The class defines attributes and the behaviour of objects'''
+
+'''To create a class we need the keyword class followed by the name and colon.Class name shuld be CamelCase.'''
+
+#Example
+class Person:
+    pass
+print(Person)
+
+#creating object by calling the class 
+p = Person()
+print(p)
+
+'''CLASS CONSTRUCTOR - a class without a constructor is not really useful in real application.we have to use constructor to make our class more useful. python has inbuilt constructor function "init()" constructor function. The init() has "self" parameter which is reference to the current instance of the class'''
+
+#Example:
+class Employee:
+    def __init__(self,name):
+        #self allows to attach paramter to the class
+        self.name = name
+
+p = Employee('Rohit')
+print(p.name)
+print(p)
+
+class Person:
+    def __init__(self,firstname,lastname,age,city,country):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age =age
+        self.city = city
+        self.country = country
+
+p = Person('Rohit','Jagave',21,'Latur','India')
+print(p.firstname)
+print(p.lastname)
+print(p.age)
+print(p.city)
+print(p.country)
+
+
+        
---
+++ Python/12-Day-Class&Object/object.py
@@ -0,0 +1,40 @@
+'''object is the instance for a class.we can create a object by calling class .'''
+
+''' object methods - Objects can have methods .The methods are functions which belongs to the object .'''
+
+#example:
+class Person:
+    def __init__(self,firstname,lastname,age,country):
+        self.firstname =firstname
+        self.lastname = lastname
+        self.age = age
+        self.country = country
+
+    def person_info(self):
+            return f'{self.firstname} { self.lastname} {self.age} years old.He lives in {self.country}'
+
+#creating a object        
+p = Person(firstname= "Rohit",
+          lastname= "Jagave",
+          age= 21,
+          country= "India"
+          )
+#calling a object with method
+print(p.person_info())
+
+
+#Object default method - deault values for your object methods .
+class Person:
+    def __init__(self,firstname='Rohit',lastname='Jagave',age=21,country='India'):
+
+        self.firstname =firstname
+        self.lastname = lastname
+        self.age = age
+        self.country = country
+
+    def person_info(self):
+            return f'{self.firstname} { self.lastname} {self.age} years old.He lives in {self.country}'
+p = Person()
+print(p.person_info)
+
+        
