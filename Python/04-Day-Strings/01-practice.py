2b2b97691cfdc524cf0798d3d3995f10500ccec6
Rohit Jagave <jagaverohit2@gmail.com>
2026-02-27 23:59:12 +0530

    Maintain streak - revision notes

---
+++ Python/04-Day-Strings/01-practice.py
@@ -0,0 +1,24 @@
+grade = "a"
+print(grade)
+print(len(grade))
+
+words  = "Practice day 1"
+print(words)
+
+str = '''Give your life for what you wanted to achieve .Failure is 100% necessary in your journey .'''
+print(str)
+
+# access string elements by index 
+word = "Vinland"
+print(word[0])
+print(word[-1])
+
+#Slice the string word
+print(word[0:3])
+print(word[4:-1]) 
+print(word[2:-2])
+print(word[0:])
+
+word = "Vinland"
+n = word[-1:4:-1]
+print(n)
