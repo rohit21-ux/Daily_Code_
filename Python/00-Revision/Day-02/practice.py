08ae80fc6f36015cd011285a0d3a013fe8548ee2
Rohit Jagave <jagaverohit2@gmail.com>
2026-03-05 23:58:18 +0530

    List revision

---
+++ Python/00-Revision/Day-02/practice.py
@@ -0,0 +1,36 @@
+num = [21,34,60,31,7,9,0,56,78]
+
+print(num[3])
+print(num[-1])
+
+print(num[0:4])
+num_greater_than_30 = num[1:4]
+print(num_greater_than_30)
+
+
+
+#num.sort()
+#print(num)
+
+num.reverse()
+print(num)
+
+num.remove(21)
+print(num)
+
+num.append(22)
+print(num)
+
+num.extend([30,4,89])
+print(num)
+
+num.sort()
+print(num)
+
+
+
+print(id(num))
+
+print(num.append(33))
+print(num)
+
