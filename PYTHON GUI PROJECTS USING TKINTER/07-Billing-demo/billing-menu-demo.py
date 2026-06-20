11552c8376dcb6b644d373a1af1b0530ebb11b41
Rohit Jagave <jagaverohit2@gmail.com>
2026-06-20 22:45:09 +0530

    Did some UI improvements today

--- PYTHON GUI PROJECTS USING TKINTER/07-Billing-demo/billing-menu-demo.py
+++ PYTHON GUI PROJECTS USING TKINTER/07-Billing-demo/billing-menu-demo.py
@@ -1,118 +1,138 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 import sys
 
 
 
 
 def do_compute():
-    # Prices for the food items 
-    samosa = 15.00    # Price of samosa
-    tea    = 5.00    # Price of tea
-    noodles = 20.00  # Price of noodles
-    bottle = 20.00    # Price of bottle
+    # Prices for the stationary
+    notebook = 40.00   # Price of notebook
+    pen      = 10.00   # Price of pen
+    pencil   = 5.00    # Price of pencil
+    eraser   = 8.00    # Price of eraser
     
 
-    qty1 = int(f1_str_var.get())
-    qty2 = int(f2_str_var.get())
-    qty3 = int(f3_str_var.get())
-    qty4 = int(f4_str_var.get())
+    qty1 = int(f1_str_var.get() or 0)
+    qty2 = int(f2_str_var.get() or 0)
+    qty3 = int(f3_str_var.get() or 0)
+    qty4 = int(f4_str_var.get() or 0)
+
+    if qty1 == 0 and qty2 == 0 and qty3 ==0 and qty4 == 0:
+        messagebox.showwarning("No order", "Please!,Place order first... ")
+        
+        return
 
     print(qty1,qty2,qty3,qty4)
 
     # Calculate total bill
-    total = (qty1 * samosa) + (qty2 * tea) + (qty3 * noodles) + (qty4 * bottle)
+    total = (qty1 * notebook) + (qty2 * pen) + (qty3 * pencil) + (qty4 * eraser)
     print(total)
     
 
 
-    # result with rupee symbol
+    # Display the total bill
     result.set(f'Total Bill: ₹{total:.2f}')
     
 
 def do_clear():
-    f1_str_var.set('')
-    f2_str_var.set('')
-    f3_str_var.set('')
-    f4_str_var.set('')
-    result.set('')
+    empty_str = ''
+    f1_str_var.set(empty_str)
+    f2_str_var.set(empty_str)
+    f3_str_var.set(empty_str)
+    f4_str_var.set(empty_str)
+    result.set(empty_str)
     
 
 def do_exit():
     sys.exit(0)
 
 root_window = Tk()
-root_window.title("BILLING MENU DEMO")
-root_window.minsize(400, 300)
-root_window.maxsize(800, 600)
-root_window.configure(bg='grey')
+root_window.title(" 🧾BILLING MENU DEMO")
+
+
+
+input_frame = ttk.Frame(root_window, padding="3 3 12 12", borderwidth=10, relief='raised')
+input_frame.grid(row=0, column=0, padx=10, pady=10,sticky=(N,W,E,S))
+input_frame.configure(relief='solid')
 
 # Labels with rupee symbol
-label_1 = ttk.Label(root_window)
-label_1.configure(text='Enter quantity of samosa (₹15):')
-label_1.grid(row=0, column=0, sticky='w', padx=10, pady=5)
+label_1 = ttk.Label(input_frame)
+label_1.configure(font=('Verdana', 8, 'bold'),text='Enter quantity of notebook (₹40):')
+label_1.grid(row=0, column=0, sticky='w', padx=10, pady=10)
 
-label_2 = ttk.Label(root_window)
-label_2.configure(text='Enter quantity of tea (₹5):')
-label_2.grid(row=1, column=0, sticky='w', padx=10, pady=5)
+label_2 = ttk.Label(input_frame)
+label_2.configure(font=('Verdana', 8, 'bold'),text='Enter quantity of pen (₹10):')
+label_2.grid(row=1, column=0, sticky='w', padx=10, pady=10)
 
-label_3 = ttk.Label(root_window)
-label_3.configure(text='Enter quantity of noodles (₹20):')
-label_3.grid(row=2, column=0, sticky='w', padx=10, pady=5)
+label_3 = ttk.Label(input_frame)
+label_3.configure(font=('Verdana', 8, 'bold'),text='Enter quantity of pencil (₹5):')
+label_3.grid(row=2, column=0, sticky='w', padx=10, pady=10)
 
-label_4 = ttk.Label(root_window)
-label_4.configure(text='Enter quantity of water bottle (₹20):')
-label_4.grid(row=3, column=0, sticky='w', padx=10, pady=5)
-
-# Result Labels
-label_5 = ttk.Label(root_window)
-label_5.configure(text='Total Bill:')
-label_5.grid(row=6, column=1, sticky='w', padx=10, pady=5)
+label_4 = ttk.Label(input_frame)
+label_4.configure(font=('Verdana', 8, 'bold'),text='Enter quantity of eraser (₹8):')
+label_4.grid(row=3, column=0, sticky='w', padx=10, pady=10)
 
 
-label_6= ttk.Label(root_window)
-label_6.configure(text='Thanks for visiting, come again!!')
-label_6.grid(row=7,column=1,sticky='w', padx=10, pady=5)
 
 # Entry
 f1_str_var = StringVar()
-f1_entry = Entry(root_window)
-f1_entry.configure(textvariable=f1_str_var, bg="#99ACFF")
-f1_entry.grid(row=0, column=1, padx=10, pady=5)
+f1_entry = Entry(input_frame)
+f1_entry.configure(font=('Consolas', 14, 'bold'),textvariable=f1_str_var, bg="#99ACFF")
+f1_entry.grid(row=0, column=1, padx=10, pady=12)
 
 f2_str_var = StringVar()
-f2_entry = Entry(root_window)
-f2_entry.configure(textvariable=f2_str_var, bg="#99ACFF")
-f2_entry.grid(row=1, column=1, padx=10, pady=5)
+f2_entry = Entry(input_frame)
+f2_entry.configure(font=('Consolas', 14, 'bold'),textvariable=f2_str_var, bg="#99ACFF")
+f2_entry.grid(row=1, column=1, padx=10, pady=12)
 
 f3_str_var = StringVar()
-f3_entry = Entry(root_window)
-f3_entry.configure(textvariable=f3_str_var, bg="#99ACFF")
-f3_entry.grid(row=2, column=1, padx=10, pady=5)
+f3_entry = Entry(input_frame)
+f3_entry.configure(font=('Consolas', 14, 'bold'),textvariable=f3_str_var, bg="#99ACFF")
+f3_entry.grid(row=2, column=1, padx=10, pady=12)
 
 f4_str_var = StringVar()
-f4_entry = Entry(root_window)
-f4_entry.configure(textvariable=f4_str_var, bg="#99ACFF")
-f4_entry.grid(row=3, column=1, padx=10, pady=5)
+f4_entry = Entry(input_frame)
+f4_entry.configure(font=('Consolas', 14, 'bold'),textvariable=f4_str_var, bg="#99ACFF")
+f4_entry.grid(row=3, column=1, padx=10, pady=12)
+
+
+button_frame = ttk.Frame(root_window, padding="3 3 12 12", borderwidth=10, relief='solid')
+button_frame.grid(row=1, column=0,padx=10,pady=10,sticky=(N,W,E,S))
+button_frame.columnconfigure(0, weight=1)
+button_frame.columnconfigure(1, weight=1)
+button_frame.columnconfigure(2, weight=1)
 
 # Buttons
-button_handle_1 = Button(root_window)
-button_handle_1.configure(text='Compute Bill', command=do_compute, bg='yellow')
-button_handle_1.grid(row=4, column=0, padx=25, pady=25)
+compute_button = Button(button_frame)
+compute_button.configure(font=('Century Gothic', 11, 'bold'),text='💰Compute Bill', command=do_compute ,bg="#78E790")
+compute_button.grid(row=0, column=0,sticky=(N,W,E,S))
 
-button_handle_2= Button(root_window)
-button_handle_2.configure( text='Clear', command=do_clear, bg='yellow')
-button_handle_2.grid(row=4, column=1, padx=25, pady=25)
+clear_button= Button(button_frame)
+clear_button.configure(font=('Century Gothic', 11, 'bold'), text='🧹Clear', command=do_clear, bg="#E8FF83")
+clear_button.grid(row=0, column=1, sticky=(N,W,E,S))
 
-button_handle_3 = Button(root_window)
-button_handle_3.configure(text='Exit', command=do_exit, bg='yellow')
-button_handle_3.grid(row=4, column=2, padx=25, pady=25)
+exit_button = Button(button_frame)
+exit_button.configure( font=('Century Gothic', 11, 'bold'),text='👋Exit', command=do_exit,bg="#E06565")
+exit_button.grid(row=0, column=2, sticky=(N,W,E,S))
+
+# output frame
+output_frame = ttk.Frame(root_window,padding= "3 3 12 12",borderwidth=10 ,relief='solid' )
+output_frame.grid(row = 2,column=0,padx=10,pady=10,sticky=(N,W,E,S))
+output_frame.columnconfigure(0,weight=1)
 
 # Result labels 
 result = StringVar()
-result_label_1 = Label(root_window)
-result_label_1.configure(textvariable=result, bg='lightgray')
-result_label_1.grid(row=6, column=1, padx=10, pady=5)
+result_label_1 = Label(output_frame)
+result_label_1.configure( font=('Georgia', 11, 'italic','bold'),textvariable=result, bg="#FF8FF9")
+result_label_1.grid(row=0, column=0, padx=10, pady=10,sticky="ew")
+
+label_6= ttk.Label(output_frame, anchor='center')
+label_6.configure( font=('Georgia', 11, 'italic','bold'),text='Thanks for visiting, come again!!', background="#78E48A",foreground="blue" )
+label_6.grid(row=1,column=0, padx=10, pady=5,sticky='ew')
+
+
 
 
 
